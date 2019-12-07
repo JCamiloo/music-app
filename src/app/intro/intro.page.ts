87a253ab283bcaa954c39351505731ae432d2e53
JCamiloo <juan.osorio@gfourmis.co>
2019-12-06 21:49:11 -0500

    guard for intro page

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -53,7 +53,7 @@ export class IntroPage implements OnInit {
 
   finish() {
     this.storage.set('isIntroShowed', true);
-    this.router.navigateByUrl("/home");
+    this.router.navigateByUrl('/home');
   }
 
 }
